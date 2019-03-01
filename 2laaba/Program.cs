d86197504e8e0a48a00dd34d139e88e7258f3a2c
Матвей Масляков <matveimaslyakov>
2019-03-01 14:38:01 +0700

    a

---
+++ 2laaba/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2laaba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
